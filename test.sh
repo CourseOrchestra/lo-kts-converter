79fe74c55040e3a8b12fcff9c9e2f01f29980ff6
fiddlededee <consulting@yandex.ru>
2023-07-29 22:50:21 +0300

    feat: Added cli parameter to run LO

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 rm -rf target
 mkdir target
 cp test.fodt_ target
-./lo-kts-converter.main.kts -i target/test.fodt_ -f pdf,docx
+./lo-kts-converter.main.kts -i target/test.fodt_ -f pdf,docx -c soffice
