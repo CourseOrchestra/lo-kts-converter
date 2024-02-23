24a3cbf2ddf312d3d779580c077ac341ad42eb7f
fiddlededee <consulting@yandex.ru>
2024-02-23 22:57:25 +0300

    feat: Added fields updating

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 rm -rf target
 mkdir target
 cp test.fodt_ target
-./lo-kts-converter.main.kts -i target/test.fodt_ -f pdf,docx -c soffice
+./lo-kts-converter.main.kts -i target/test.fodt_ -f pdf,docx -c soffice -t
